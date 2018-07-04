1501cc6166b3b12522a96540a5852c6bd27f1a82
marartrix <maratrix@163.com>
2018-07-04 09:05:30 +0800

    数据类型与变量

---
+++ code/type.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+class A:
+    pass
+
+class B(A):
+    pass
+
+print(isinstance(A(), A))
+print(type(A()) == A)
+print(isinstance(B(), A))
+print(type(B()) == A)
