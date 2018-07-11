ae031477edce12f8dca071632cb6f2759c9c4d2f
marartrix <maratrix@163.com>
2018-07-11 09:09:22 +0800

    函数

--- code/func.py
+++ code/func.py
@@ -23,3 +23,25 @@ more_params(name = 'hanson', age = 18)
 d = {'name':'Maratrix', 'hobby': 'python', 'sex':'male'}
 
 more_params(**d)
+
+def person(name, age, *, city, job = 'it...', **kw):
+    print(name, age, city, job, kw)
+
+person('hanson', 18, city = 'BJ', adr = 'beijing')
+
+def foo(a, b, c=0, *args, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'd =', d, 'kw =', kw)
+
+foo(*[1, 2, 3, 4, 5], **{'d':'dd', 'e':'ee', 'f':'ff'})
+
+
+
+sum = lambda a, b: a + b
+
+print(sum(1, 2))
+print(sum(3, 4))
+
+li = [lambda :x for x in range(10)]
+
+print(li, type(li))
+print(type(li[0]))
